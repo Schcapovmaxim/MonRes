72a3ddad22c30b4f6c6ccfdbc282b3ab40d7ded7
Max <maksimsapov0456@gmail.com>
2025-03-08 15:08:25 +0500

    Add project files.

---
+++ FrontResMon/Form1.cs
@@ -0,0 +1,16 @@
+using MetroFramework.Forms;
+namespace FrontResMon
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
