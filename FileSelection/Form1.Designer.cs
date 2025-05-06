67fee8f78957ab17d851579fa7f2184a1934719f
Max <maksimsapov0456@gmail.com>
2025-05-06 19:54:18 +0500

    ye

--- FileSelection/Form1.Designer.cs
+++ FileSelection/Form1.Designer.cs
@@ -116,6 +116,7 @@
             this.Controls.Add(this.buttonChoiceFile);
             this.Name = "Form1";
             this.Text = "FileSelection";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
