39713835b1939b27a66062175415ad4333ee487c
IYI4k <IYI4k@yandex.ru>
2025-07-04 00:05:15 +0500

    Now output textBox is sizeable

--- FileSelection/MainWindow.Designer.cs
+++ FileSelection/MainWindow.Designer.cs
@@ -148,7 +148,9 @@
             // 
             // OutPutTextBox_BVP
             // 
-            this.OutPutTextBox_BVP.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutPutTextBox_BVP.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             // 
             // 
             // 
